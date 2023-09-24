be2cb0da219e53471b7e37fcc1aa9739018559a5
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-09-24 10:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here!
+// Your code with the corrected condition
+let itemCount = 1;
+const container = document.querySelector("#infi-list");
+
+const createListItems = (count = 0) => {
+    for (let i = 0; i < count; i++) {
+        const li = document.createElement("li");
+        li.textContent = "Item " + itemCount++;
+        container.appendChild(li);
+    }
+}
+
+createListItems(10);
+
+container.addEventListener("scroll", (event) => {
+    const e = event.target;
+
+    if (e.scrollTop + e.offsetHeight + 100 > e.scrollHeight) {
+        createListItems(10);
+        //console.log("ADD");
+    }
+});
 
